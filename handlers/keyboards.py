de92a40c6cc71064606aee620cc4ce40fb08435e
mysin.i <mysin_id@mail.ru>
2023-02-16 13:04:53 +0300

    refactoring keyboards

---
+++ handlers/keyboards.py
@@ -0,0 +1,65 @@
+from telegram import (
+    InlineKeyboardButton,
+    InlineKeyboardMarkup
+)
+
+from .config_handlers import *
+
+
+def get_district_keyboard():
+    keyboard = [
+        [InlineKeyboardButton(d, callback_data=data)]
+        for data, d in DISTRICT_DICT.items()
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_users_count_keyboard():
+    keyboard = [
+        [InlineKeyboardButton(str(x), callback_data=str(x))]
+        for x in range(1, 4)
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_max_users_count_keyboard(context):
+    keyboard = [
+        [InlineKeyboardButton(str(x), callback_data=str(x))]
+        for x in range(context.user_data['users_count'] + 1, 5)
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_class_keyboard():
+    keyboard = [
+        [
+            InlineKeyboardButton(d, callback_data=data)
+        ] for data, d in CLASS_DICT.items()
+    ]
+
+    return InlineKeyboardMarkup(keyboard)
+
+
+def get_drives_list_keyboard(data):
+    keyboard = [
+        [
+            InlineKeyboardButton(f"Вариант {i + 1} - "
+                                 f"{x['users_count']} из "
+                                 f"{x['max_users_count']} человек",
+                                 callback_data=x['admin_username'])
+        ] for i, x in enumerate(data['drives_list'])
+    ]
+    # TODO отмена
+    return InlineKeyboardMarkup(keyboard) if keyboard else False
+
+
+def get_create_drive_keyboard():
+    keyboard = [[InlineKeyboardButton(f"Создать новую поездку",
+                                      callback_data='new_drive')],
+                [InlineKeyboardButton(f"Запустить поиск еще раз",
+                                      callback_data='start')]]
+
+    return InlineKeyboardMarkup(keyboard)
