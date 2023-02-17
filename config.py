a45ce897d02268949037ffed543b14dbe9b103b1
mysin.i <mysin_id@mail.ru>
2023-02-17 10:54:36 +0300

    rework config

--- config.py
+++ config.py
@@ -1,3 +1,18 @@
+import os
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN", "")
+
+
+BASE_DIR = Path(__file__).resolve().parent
+TEMPLATES_DIR = BASE_DIR / "templates"
+
+
 DISTRICT_DICT = {'1': 'Приморский',
                  '2': 'Петроградский'}
 
